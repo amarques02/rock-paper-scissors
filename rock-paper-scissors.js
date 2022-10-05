4a9d9976b263f7114fc4bd0f1bdb271761f44406
Antonio thadeu Marques neto <amarques@c2r4s1.42lisboa.com>
2022-10-05 18:23:54 +0100

    Create game with prompt implementation and printing on console

---
+++ rock-paper-scissors.js
@@ -0,0 +1,44 @@
+let	choices = ['rock', 'paper', 'scissors'];
+function getComputerChoice() {
+	return (choices[Math.floor(Math.random() * choices.length)])
+}
+
+ function playRound(playerSelection, computerSelection) {
+	if (playerSelection == 'rock'){
+		if (computerSelection == 'paper')
+			return ("You Lose! Paper beats Rock")
+		else if (computerSelection == 'scissors')
+			return ("You Win! Rock beats Scissors")
+		else
+			return ("Draw!")
+	}
+	else if (playerSelection == 'paper'){
+		if (computerSelection == 'rock')
+			return ("You Win! Paper beats Rock")
+		else if (computerSelection == 'scissors')
+			return ("You Lose! scissors beats paper")
+		else
+			return ("Draw!")
+	}
+	else {
+		if (computerSelection == 'paper')
+			return ("You Win! Scissors beats Paper")
+		else if (computerSelection == 'rock')
+			return ("You Lose! Rock beats Scissors")
+		else
+			return ("Draw!")
+	}
+}
+while (1) {
+	let playerSelection = window.prompt("Pick your weapon!");
+	playerSelection = playerSelection.toLowerCase();
+	while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != 'scissors') {
+		playerSelection = window.prompt("Woops! That\'s not a valid choice!\nTry again");
+		playerSelection = playerSelection.toLowerCase();
+	}
+	const computerSelection = getComputerChoice();
+	console.log(playRound(playerSelection, computerSelection));
+	let close = window.prompt("Try again or type \'close\' to exit the game");
+	if (close == 'close')
+		break;
+}	
